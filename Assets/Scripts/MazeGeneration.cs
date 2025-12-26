e3e1ae3e28e6c8e33ef521baf05277fd9a9fc075
FireKnight76 <fireknight593@gmail.com>
2025-12-26 16:06:44 +0100

    Created method for creating cells and storing them
    Created a multidimensional array to store all the created cells.
    The cells are created in the method GenerateGrid() and automatically stored.

--- Assets/Scripts/MazeGeneration.cs
+++ Assets/Scripts/MazeGeneration.cs
@@ -7,6 +7,9 @@ public class MazeGeneration : MonoBehaviour
     [SerializeField] TMP_InputField xSize;
     [SerializeField] TMP_InputField zSize;
 
+    //the multidimensional array to contain the createed cells
+    MazeCell[,] mazeGrid;
+
     int x;
     int z;
 
@@ -15,12 +18,33 @@ public class MazeGeneration : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Mouse1))
         {
-            InputToInt(xSize, zSize);
-            Debug.Log($"x = {x} and z = {z}");
+            if((x >= 10 && x <= 250) && (z >= 10 && z <= 250))
+            {
+                InputToInt(xSize, zSize);
+                GenerateGrid(x, z);
+
+            }
         }
     }
 
 
+    void GenerateGrid(int  x, int z)
+    {
+        mazeGrid = new MazeCell[x, z];
+
+        for (int i = 0; i < mazeGrid.GetLength(0); i++)
+        {
+            for (int j = 0; j < mazeGrid.GetLength(1); j++)
+            {
+                MazeCell currentCell = Instantiate(cell, new Vector3(i - (x / 2f), 0, j - (z /2f)), Quaternion.identity);
+
+                mazeGrid[i, j] = currentCell;
+
+            }
+        }
+    }
+
+    //method to turn the input of the input fields into integers for the maze generation
     void InputToInt(TMP_InputField width, TMP_InputField height)
     {
         x = int.Parse(width.text);
