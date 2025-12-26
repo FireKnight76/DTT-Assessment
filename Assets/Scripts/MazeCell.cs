4f193c7b5ac46329105fabd6e0d456b470534e65
FireKnight76 <fireknight593@gmail.com>
2025-12-26 15:29:22 +0100

    Replaced folder with Unity preset to fix visual studio, and created MazeCell script and prefab
    The inside of the folder has been replaced with a preset from Unity since autocorrect was not working.
    I also created the MazeCell prefab and script to use in the generation of the maze later

---
+++ Assets/Scripts/MazeCell.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public enum cellState
+{
+    Available,
+    Passed,
+    Completed
+}
+
+public class MazeCell : MonoBehaviour
+{
+    [SerializeField] private GameObject PosXWall; 
+    [SerializeField] private GameObject NegXWall;
+    [SerializeField] private GameObject PosZWall;
+    [SerializeField] private GameObject NegZWall;
+    [SerializeField] private MeshRenderer floor;
+
+    public void RemoveRightWall()
+    {
+        PosXWall.gameObject.SetActive(false);
+    }
+
+    public void RemoveLeftWall()
+    {
+        NegXWall.gameObject.SetActive(false);
+    }
+
+    public void RemoveFrontWall()
+    {
+        PosZWall.gameObject.SetActive(false);
+    }
+
+    public void RemoveBackWall()
+    {
+        NegXWall.gameObject.SetActive(false);
+    }
+
+
+
+}
